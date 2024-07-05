de058ea5fa0ec01cbf52bfd38b4bc3db4fc7861f
valentin <appeehal@gmail.com>
2024-07-05 13:39:11 +0100

    commiting changes

---
+++ submitone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=my_lammps_tensile_simulation
+#SBATCH --partition=test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=00:01:00
+#SBATCH --mem=190GB
+#SBATCH --account=EEME030304
+
+source .bashrc
+
+# cd your directory
+
+srun -n 24 --cpu-bind=cores --mpi=pmix_v3 lmp_mpi -in in.tensile_strength_test.lmp > log_output.txt 
+
+# lmp -in in.tensile_strength_test.lmp
+
